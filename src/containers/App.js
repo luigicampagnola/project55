a5676f70d99580fcb040705e1b148466155794b2
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-13 19:32:34 +0200

    a

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   //loading users
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("")
       .then((response) => response.json())
       .then((users) => this.setState({ robots: users }));
   }
